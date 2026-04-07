ad0536fe7adb3c8c99cc4ac86c2cf17654f5cfa0
Jubayer <jubayer8221@gmail.com>
2026-04-07 10:27:03 +0600

    api/product

--- RebelsClothing/Program.cs
+++ RebelsClothing/Program.cs
@@ -1,64 +1,4 @@
-﻿//using Microsoft.EntityFrameworkCore;
-//using RebelsClothing.Data;
-
-//var builder = WebApplication.CreateBuilder(args);
-
-//// Add services to the container
-//builder.Services.AddControllers();
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
-
-
-//// ✅ Add CORS policy
-//builder.Services.AddCors(options =>
-//{
-//    options.AddPolicy("AllowFrontend",
-//        policy =>
-//        {
-//            //policy
-//            //    .WithOrigins("http://localhost:5173") // your Next.js
-//            //    .WithOrigins("http://localhost:3000") // your Next.js app
-//            //    .WithOrigins("https://rebels-admin-dashboard.vercel.app")
-//            //    .AllowAnyMethod()
-//            //    .AllowAnyHeader();
-//            policy
-//            .WithOrigins(
-//            "http://localhost:5173",
-//            "https://rebels-admin-dashboard.vercel.app"
-//)
-//.AllowAnyMethod()
-//.AllowAnyHeader();
-//        });
-//});
-
-//builder.WebHost.UseUrls("http://0.0.0.0:1920");
-
-//// ✅ Add DB context
-//builder.Services.AddDbContext<ApplicationDbContext>(options =>
-//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
-//);
-
-//var app = builder.Build();
-
-//// Configure the HTTP request pipeline
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
-
-//app.UseHttpsRedirection();
-
-//// ✅ Use CORS here BEFORE MapControllers
-//app.UseCors("AllowFrontend");
-
-//app.UseAuthorization();
-
-//app.MapControllers();
-
-//app.Run();
-
-
+﻿
 using Microsoft.EntityFrameworkCore;
 using RebelsClothing.Data;
 
@@ -86,7 +26,9 @@ builder.Services.AddCors(options =>
 });
 
 // ✅ Bind server to port 1920 for all networks
-builder.WebHost.UseUrls("http://0.0.0.0:1920");
+//builder.WebHost.UseUrls("http://0.0.0.0:1920");
+//var port = Environment.GetEnvironmentVariable("PORT") ?? "10000";
+//builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
 
 // ✅ Database connection
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
